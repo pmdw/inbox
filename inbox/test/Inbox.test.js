11d17f604418e6e04d8aa78fb54660683afb2f73
pmdw <39948389+pmdw@users.noreply.github.com>
2020-05-25 17:06:47 +0100

    Test set up

--- inbox/test/Inbox.test.js
+++ inbox/test/Inbox.test.js
@@ -6,3 +6,33 @@ const Web3 = require('web3'); //constructor function
 
 //instance of web3
 const web3 = new Web3(ganache.provider());
+
+//mocha Functions 
+// (1)it[assert and test]  
+// (2)describe[group together it(1)] 
+// (3)beforeEach [set up ]
+
+// testing class
+class Car {
+	park (){
+		return 'stopped';
+	}
+	
+	drive(){
+		return 'vroom';
+	}
+}
+
+//parameters (test group name, arrow function)
+describe('CarUnitTest', () => {
+			//to test park function
+			it('ParkedTest',() =>{
+				//test set up and assertion logic
+				const car = new Car();
+				assert.equal(car.parked(),'stopped');
+			})
+			
+			
+		}
+
+		); //end describe
